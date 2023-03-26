11fc7088a6444fd39aa3112373dfb2cd8517110d
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-03-26 17:31:17 +0900

    create Category Folder and JS/CSS Files

---
+++ src/Components/Main/Category/Category.js
@@ -0,0 +1,13 @@
+import './Category.css'
+
+function Category() {
+    return (
+        <div className='Category'>
+            <footer class="Category">
+                CodeStates
+            </footer>
+        </div>
+    )
+}
+
+export default Category;
