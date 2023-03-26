54ead3ab916f8eb4a535b954b94363b824f8e1bc
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-03-26 17:58:11 +0900

    design(Category): Grid 배치 완성

--- src/Components/Main/Category/Category.js
+++ src/Components/Main/Category/Category.js
@@ -1,11 +1,20 @@
+import {Link} from 'react-router-dom'
+
 import './Category.css'
 
 function Category() {
     return (
         <div className='Category'>
-            <footer class="Category">
-                CodeStates
-            </footer>
+            <section className="category__wrapper">
+                <div className="grid__item">
+                    <Link to="/board?category=전체질문&page=1">전체 질문</Link>
+                </div>
+                <div className="grid__item">급해요 급해!</div>
+                <div className="grid__item">라이브 세션 질문</div>
+                <div className="grid__item">핫 토픽</div>
+                <div className="grid__item">느긋한 질문</div>
+                <div className="grid__item">기타 문의</div>
+            </section>
         </div>
     )
 }
