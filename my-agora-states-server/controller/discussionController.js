ef2dd2a8c510a6ca74fc9dece7313ba1df1f4226
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-04-07 19:36:16 +0900

    feat: 최근 게시물 조회 기능 추가

--- my-agora-states-server/controller/discussionController.js
+++ my-agora-states-server/controller/discussionController.js
@@ -3,12 +3,10 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    console.log("findAll")
     return res.json(discussionsData);
   },
 
   findById: (req, res) => {
-    console.log("findById")
     let filteredDiscussion = discussionsData.filter((discussion) =>{
         return discussion.id === Number(req.params.id);
     })
@@ -16,6 +14,23 @@ const discussionsController = {
       return res.status(200).json(...filteredDiscussion);
     else
       return res.status(404).send('존재하지 않습니다.');
+  },
+
+  findByCategory: (req, res) => {
+    const { category } = req.params;
+
+    let filteredDiscussions = [];
+
+    if(category === 'recent'){
+      for(let i=0; i<8; i++){
+        filteredDiscussions.push(discussionsData[i]);
+      }
+    }
+    if(category === 'hotTopic'){
+
+    }
+
+    return res.status(200).json(filteredDiscussions);
   }
 
 };
