dd6e3c11ae639e1024f616fcedb9fa6fe7f9bdbc
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-04-07 19:32:16 +0900

    design: Discussion 컴포넌트 추가

--- my-agora-states-client/src/Components/Discussion/Discussion.js
+++ my-agora-states-client/src/Components/Discussion/Discussion.js
@@ -1,10 +1,21 @@
 import './Discussion.css'
 
-function Board_Item() {
+function Board_Item(props) {
+    const { id, createdAt, title, author, avatarUrl} = props.data;
+
     return (
         <div className='Discussion'>
             <div className='discussion__container'>
-
+                <div className='discussion__avatar__wrapper'>
+                    <img src={avatarUrl} alt="profile"/>
+                </div>
+                <div className='discussion__content'>
+                    <div className='discussion__title'>{title}</div>
+                    <div className='discussion__info'>
+                        <div className='discussion__author'>{author}</div>
+                        <div className='discussion__date'>{createdAt}</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
