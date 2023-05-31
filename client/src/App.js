bc23a34a5b171fb36884254d0811c297f1146405
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-05-31 22:58:32 +0900

    feat/Header 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './common/components/Header'
 import MainPage from './Pages/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header/>
             <Routes>
                 <Route path="/" element={<MainPage/>}></Route>
                 <Route></Route>
