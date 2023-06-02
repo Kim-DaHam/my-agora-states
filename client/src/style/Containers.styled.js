003551206f058becab94a701fa9ad1decea1623f
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-02 23:01:24 +0900

    feat/공통 Container 컴포넌트 생성

---
+++ client/src/style/Containers.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import '../common/data/global-style.css'
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--color-night-bg);
+`;
+
+export const Wrapper = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
